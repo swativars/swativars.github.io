f88ef3c34725913f5b926d9c64b9ddb56bc86d70
swativars <86065223+swativars@users.noreply.github.com>
2022-01-06 17:46:51 +0530

    Add files via upload

--- assets/js/common.js
+++ assets/js/common.js
@@ -9,3 +9,29 @@ document.onreadystatechange = function () {
         document.documentElement.style.overflow = 'auto';
     }
 };
+
+$("[data-trigger]").on("click", function () {
+    console.log("bv jhhhhhhhhhhhhhhhhhh")
+    var trigger_id = $(this).attr('data-trigger');
+    $(trigger_id).addClass("show");
+    console.log("fdfegrefge", trigger_id);
+    $('html').addClass("offcanvas-active");
+    $('html').css('overflow', 'hidden');
+});
+
+//close button
+$(".btn-close").click(function (e) {
+    if (e.target.parentNode.className !== 'navbar-toggler') {
+        $(".navbar-collapse").removeClass("show");
+        $("html").removeClass("offcanvas-active");
+        $('html').css('overflow', 'auto');
+    }
+});
+var html = document.querySelector('html');
+window.onclick = function (e) {
+    if (e.target === html) {
+        $(".navbar-collapse").removeClass("show");
+        $("html").removeClass("offcanvas-active");
+        $('html').css('overflow', 'auto');
+    }
+}
